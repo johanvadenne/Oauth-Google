469ee10239e7a00aa78b7185c80df4503241b531
wolffire18 <johanvadenne@gmail.com>
2024-03-04 14:20:34 +0100

    1.0

---
+++ config.php
@@ -0,0 +1,6 @@
+<?php
+
+define('GOOGLE_ID', '1094594225300-cn4o96rfpfqs25f5umjl5fv1hlq9lmp5.apps.googleusercontent.com');
+define('GOOGLE_SECRET', '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');
+
+?>
---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Google</title>
+</head>
+<body>
+    <h1>Google</h1>
+    <a href="https://accounts.google.com/o/oauth2/v2/auth?
+ scope=https%3A//www.googleapis.com/auth/drive.metadata.readonly&
+ access_type=offline&
+ include_granted_scopes=true&
+ response_type=code&
+ state=state_parameter_passthrough_value&
+ redirect_uri=https%3A//oauth2.example.com/code&
+ client_id=client_id"></a>
+</body>
+</html>
